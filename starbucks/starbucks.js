6517033fe06a58c66c8f8bd3305437069fe2ff3c
haenanim121@gmail.com <haenanim121@gmail.com>
2023-09-25 10:54:30 +0900

    commit

--- starbucks/starbucks.js
+++ starbucks/starbucks.js
@@ -1,13 +1,14 @@
 window.addEventListener('scroll', function () {
-  let fav_text = document.querySelectorAll('.fav-text');
+  let favTexts = document.querySelectorAll('.fav-text');
   let value = scrollY;
   if (value > 400) {
-    fav_text.forEach((item) => {
-      item.style.animation = 'slide-backward 2s ease-in-out';
-    });
+    favTexts.forEach(
+      (item) =>
+        (item.style.animation = 'slide-backward 2s ease-in-out forwards')
+    );
   } else {
-    fav_text.forEach((item) => {
-      item.style.animation = 'slide-forward 2s ease-in-out';
-    });
+    favTexts.forEach(
+      (item) => (item.style.animation = 'slide-forward 2s ease-in-out')
+    );
   }
 });
